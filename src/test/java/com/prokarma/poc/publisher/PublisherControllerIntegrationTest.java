c91f7139c343498611debb410fa88ebfea4aa3f0
bakavar1 <bargavakumar.akavaram1@t-mobile.com>
2021-01-21 16:36:05 +0530

    integration test, splunk integration

--- src/test/java/com/prokarma/poc/publisher/PublisherControllerIntegrationTest.java
+++ src/test/java/com/prokarma/poc/publisher/PublisherControllerIntegrationTest.java
@@ -1,42 +1,70 @@
-/*
 package com.prokarma.poc.publisher;
 
+import com.prokarma.poc.publisher.model.CustomerDetails;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = PublisherApplication.class,
+import javax.annotation.PostConstruct;
+
+import static io.restassured.RestAssured.given;
+
+@SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+@TestPropertySource(
+        locations = "classpath:application.properties")
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 class PublisherControllerIntegrationTest {
 
+    private static final String AUTH_TOKEN_URI = "/oauth/token";
+    private static final String RESOURCE_URL = "/publisher/v1/customerDetails";
     @LocalServerPort
     private int port;
+    private String uri;
 
-    TestRestTemplate restTemplate = new TestRestTemplate();
+    @PostConstruct
+    public void init() {
+        uri = "http://localhost:" + port;
+    }
 
     @Test
-    void contextLoads() {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Authorization", "68d002f3-4370-40f7-a92d-dead52d96cb8");
-        httpHeaders.add("Activity-Id", "12345");
-        httpHeaders.add("Application-Id", "12345");
+    public void givenMovie_whenMakingPostRequestToMovieEndpoint_thenCorrect() {
 
-        HttpEntity<String> entity = new HttpEntity<String>(null, httpHeaders);
+        CustomerDetails customerDetails = TestUtil.createCustomerDetails();
 
-        restTemplate.exchange(
-                createURLWithPort("/publisher/v1/customerDetails"),
-                HttpMethod.POST, entity, String.class);
+
+        String accessToken =
+                given()
+                        .auth().preemptive().basic("acme", "acmesecret")
+                        .contentType("application/x-www-form-urlencoded")
+                        .formParam("grant_type", "client_credentials")
+                        .formParam("scope", "all")
+                        .when()
+                        .post(uri + AUTH_TOKEN_URI)
+                        .then()
+                        .assertThat()
+                        .statusCode(HttpStatus.OK.value())
+                        .extract()
+                        .path("access_token");
+        given().auth()
+                .oauth2(accessToken)
+                .header("Activity-Id", "12355")
+                .header("Application-Id", "12355-455")
+                .contentType(ContentType.JSON)
+                .body(customerDetails)
+                .post(uri + RESOURCE_URL)
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value());
+
     }
 
-    private String createURLWithPort(String uri) {
-        return "http://localhost:" + port + uri;
-    }
+
 }
-*/
